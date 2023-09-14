5b786a203c5e052daf6ed2bbbee9cb64311431e1
szt2otokov <szt2otokov@vasvari.org>
2023-09-14 13:12:46 +0200

    it works

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int codeHours = 6*5*17;
+
+        System.out.println("The attendee codes " + codeHours + "h per semester");
+
+        float codePc = 52f*17f/codeHours;
+        System.out.println("They spend " + codePc + "% of their work time with coding");
     }
 }
